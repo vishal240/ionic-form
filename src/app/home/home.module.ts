807af9715b97bc15baf7ea2a08d07f7b3ba8e689
vishal240 <vishalprajapat240@gmail.com>
2022-12-05 18:59:35 +0530

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
-
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    FormsModule, ReactiveFormsModule,
     IonicModule,
     HomePageRoutingModule
   ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  items: FormArray;
+  orderForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+  }
+  get orderFormControl(): FormArray {
+    return this.orderForm.get("items") as FormArray;
+  }
+  addItem(): void {
+    this.items = this.orderForm.get('items') as FormArray;
+    this.items.push(this.createItem());
+  }
+  removeItem(i: any){
+    this.items.removeAt(i)
+  }
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      items: this.formBuilder.array([])
+    });
+    this.addItem()
+  }
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}')]],
+      address: ['', [Validators.required, Validators.minLength(5)]],
+      phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10),Validators.pattern('^(0|[1-9][0-9]*)$')]],
+    });
+  }
+  onSubmit(){
+    console.log(this.items.value)
+  }
 }
